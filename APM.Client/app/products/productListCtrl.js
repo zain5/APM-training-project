0e528ef2b4b42dfb3f5eb1bc081553cfd8c3ece6
zain5 <zainyarkhan5@gmail.com>
2016-05-03 17:56:41 -0400

    Initial Check-in of the training project of Angular Front-to-Back course by Deborah Kurata

---
+++ APM.Client/app/products/productListCtrl.js
@@ -0,0 +1,15 @@
+(function () {
+    "use strict";
+    angular
+        .module("productManagement")
+        .controller("ProductListCtrl",
+                     ["productResource", ProductListCtrl]);
+
+    function ProductListCtrl(productResource) {
+        var vm = this;
+
+        productResource.query(function(data) {
+            vm.products = data;
+        });
+    }
+}());
---
+++ APM.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using APM.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace APM.WebApi.Controllers
+{
+    [EnableCorsAttribute("http://localhost:50856", "*", "*")]
+    public class ProductsController : ApiController
+    {
+        private static ProductRepository repo = new ProductRepository();
+
+        // GET: api/Products
+        public IEnumerable<Product> Get()
+        {
+            return repo.Retrieve();
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
